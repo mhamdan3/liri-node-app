18bbb6127748427739ae1bcaf3f82867b8ded8aa
Mahdi Hamdan <hamdani0430@gmail.com>
2018-04-10 16:32:38 -0400

    .

---
+++ keys.js
@@ -0,0 +1,16 @@
+//console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'OmdZ4nLyHUWyWwjzF3vcHdlLN',
+  consumer_secret: 'RNstKeCQAeGHzCdPlOsIe69cFWRYMa73D5iAm1CTqb1adznvgw',
+  access_token_key: '828433716640886784-1oBGa1JZ1WmtZ6vCvr3REg27i9KajvL',
+  access_token_secret: 'YfNwvPYVDc9PFtQLBsE83dT150D2UpJjdpKgIJTDu7WVo',
+}
+
+exports.tmdbKey = '677a1dec';
+
+exports.spotify = {
+  id: '433a166b08604cd69e1952aea0e1caff',
+  secret: 'a5bbf420892c4f39811cdf768a494fbb',
+};
+
